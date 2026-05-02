356a53ea1abfcc80d05238df03bbea4bacaeda69
johsua092-ui <johsua092@gmail.com>
2026-05-03 02:29:36 +0700

    Final fix - Overwrite default Next.js and fix pathing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/mtk-simple-',
+  assetPrefix: '/mtk-simple-/',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
